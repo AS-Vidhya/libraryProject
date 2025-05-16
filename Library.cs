3206ab312ce03be9eacbfcef8b643a53fb835de6
Srividhya <assrividhya113@gmail.com>
2025-05-16 10:34:29 +0530

    Add Library model and EF DbContext

---
+++ Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace libraryProject.Models
+{
+	public class Library
+	{
+		[Key]
+		public int BookId { get; set; }
+		[Required]
+		public string BookName { get; set; }
+		[Required]
+		public string Author { get; set; }
+		public   int year { get; set; }
+	}
+}
---
+++ LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace libraryProject.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("LibraryContext") { }
+
+        public DbSet<Library> Libraries { get; set; }
+    }
+
+}
