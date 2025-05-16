4f28534268f0eaaeb4309d5d47bb02217189f80e
Srividhya <assrividhya113@gmail.com>
2025-05-16 13:00:12 +0530

    Add initial EF migration files

---
+++ 202505060647033_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using static System.Data.Entity.Infrastructure.Design.Executor;
+using System.Data.Entity;
+
+namespace libraryProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Libraries",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        BookName = c.String(nullable: false),
+                        Author = c.String(nullable: false),
+                        year = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Libraries");
+        }
+    }
+}
